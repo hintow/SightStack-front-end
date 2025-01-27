c59083006fe89bf6a15c8032438b28bf047392c9
Weixi He <heweixiguge@gmai.com>
2025-01-27 11:10:38 -0500

    MianHome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import LogIn from './components/LogIn';
 import NavBar from './components/NavBar.tsx';
+import MianHome from './components/MainHome.tsx';
 // import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 // imoprt Home from './components/Home';
 
@@ -18,6 +19,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <MianHome />
       <LogIn />
       hello! You are about to see a wondeful site here!!!
     </div>
---
+++ src/components/MainHome.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import './MianHome.css';
+
+
+interface Grade {
+  label: string;
+  className: string;
+}
+
+const MianHome: FC = () => {
+
+  const grades: Grade[] = [
+    { label: "Pre-K", className: "pre-k" },
+    { label: "Grade K", className: "grade-k" },
+    { label: "Grade 1", className: "grade-1" },
+    { label: "Grade 2", className: "grade-2" },
+    { label: "Grade 3", className: "grade-3" },
+    { label: "Grade 4", className: "grade-4" },
+    { label: "Grade 5", className: "grade-5" },
+    { label: "Grade 6+", className: "grade-6" },
+  ];
+
+  return (
+    <div>
+      <div className="main-circle">
+        <span className="sun-text">Daily game</span>
+      </div>
+
+      <div className="grade-container">
+        {grades.map((grade, index) => (
+          <div key={index} className={`grade-circle ${grade.className}`}>
+            {grade.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MianHome;
