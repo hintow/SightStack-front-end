3a6b906e069ec93299f86aa953dea8e22f9c54f1
Wei <qwbu22@gmail.com>
2025-01-28 20:12:24 -0800

    fix: preview allow any host name

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 5000, // Specify the port for the preview server
-    host: '0.0.0.0' // Specify the host for the preview server
+    host: '0.0.0.0',
+    allowedHosts: true,
   }
 });
