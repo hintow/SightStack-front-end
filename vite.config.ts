47c5e1327748c4e7aa33f89fd42f46a6290fbc02
Wei <qwbu22@gmail.com>
2025-01-28 20:07:52 -0800

    fix: update preview host and port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 5000, // Specify the port for the preview server
+    host: '0.0.0.0' // Specify the host for the preview server
+  }
 });
